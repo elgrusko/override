19ca8cb383cea8ca17a4a13bed1801fa3472f23e
elgrusko <45894554+elgrusko@users.noreply.github.com>
2020-10-21 13:58:51 +0200

    Update source.c

--- level04/Ressources/source.c
+++ level04/Ressources/source.c
@@ -11,8 +11,8 @@ int         main(void)
     char    buf[32];                                           
 
     memset(buf, '\0', 32);
-    int     ptrace_ret = 0;                                     
-    int     stat_loc = 0;                                       
+    int     ret = 0;                                     
+    int     state = 0;                                       
     
     if (pid == 0)                                               // FILS
     {
@@ -29,8 +29,8 @@ int         main(void)
                     puts("child is exiting...");
                     return (0);
             }
-            ptrace_ret = ptrace(3, pid, 44, NULL)
-            if (ptrace_ret == 11) {                         // exec()
+            ret = ptrace(3, pid, 44, NULL)
+            if (ret == 11) {                         // exec()
                puts("no exec() for you");
                kill(pid, SIGKILL);
             }            
